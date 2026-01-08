783573211f95dddbc730b7716078efc3778602ac
Sonia <01200993@pw.edu.pl>
2026-01-08 21:26:40 +0100

    Implemented function check_int() in order to check all integers given by user in one function

--- simulator.py
+++ simulator.py
@@ -28,35 +28,46 @@ class Simulator:
         print("1 - Białe tło")
         print("2 - Własny czarno-biały obraz")
         print("3 - Losowo wygenerowany czarno-biały obraz")
-        try:
-            mode = int(input("Twój wybór (1-3): "))
-        except ValueError:
-            print("Taki tryb nie istnieje.")
-        if mode == 2:
-            path = input("Podaj ścieżkę do obrazka: ")
-            if os.path.exists(path):
-                try:
-                    self.image_sel = True
-                    img = Image.open(path).convert('RGB')
-                    img = img.convert('1').convert('RGB')
-                    self.users_image = img
-                    self.window_width, self.window_height = img.size
-                except Exception:
-                    print("Plik jest uszkodzony")
+        mode = int(input("Twój wybór (1-3): "))
+        if self.check_int(mode, 'mode'):
+            if mode == 2:
+                path = input("Podaj ścieżkę do obrazka: ")
+                if os.path.exists(path):
+                    try:
+                        self.image_sel = True
+                        img = Image.open(path).convert('RGB')
+                        img = img.convert('1').convert('RGB')
+                        self.users_image = img
+                        self.indow, self.w_height = img.size
+                    except Exception:
+                        print("Plik jest uszkodzony")
+                else:
+                    print("Nie znaleziono pliku.")
             else:
-                print("Nie znaleziono pliku.")
-        else:
-            try:
                 self.window_width = int(input("Podaj szerokość planszy: "))
                 self.window_height = int(input("Podaj wysokość planszy: "))
-            except ValueError:
-                print("Podane wymiary nie są poprawne")
-            if mode == 3:
-                try:
-                    self.chance_black = int(input("Podaj % czarnych pól: "))
-                except ValueError:
-                    self.chance_black = 20
-        try:
-            self.steps = int(input("Podaj liczbę kroków symulacji: "))
-        except ValueError:
-            print("Błędna liczba!")
+                if self.check_int(self.window_width) \
+                        and self.check_int(self.window_height):
+                    if mode == 3:
+                        chance = int(input("Podaj czarnych pól: "))
+                        if self.check_int(chance, 'chance'):
+                            self.chance_black = chance
+                else:
+                    raise ValueError('Podana wartość nie jest poprawna')
+        else:
+            raise ValueError('Podana wartość nie jest poprawna')
+        steps = int(input("Podaj liczbę kroków symulacji: "))
+        if self.check_int(steps):
+            self.steps = steps
+        else:
+            raise ValueError('Podana wartość nie jest poprawna')
+
+    def check_int(self, input, which_value='standard_int'):
+        '''
+        Sprawdza liczbę, podaną przez użytkownika,zgodnie z wymaganiami
+        '''
+        if which_value == 'mode':
+            return input in [1, 2, 3]
+        elif which_value == 'chance':
+            return 0 <= input <= 100
+        return input > 1
